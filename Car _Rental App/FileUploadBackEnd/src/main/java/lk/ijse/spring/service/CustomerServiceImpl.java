cff3102e33c94b0c405c905684fa8bf583cc59e4
Amayuru Indeewara <indeewara5678@gmail.com>
2022-07-14 03:17:59 +0530

    fixed tight coupling issue using interface orientation

--- Car _Rental App/FileUploadBackEnd/src/main/java/lk/ijse/spring/service/CustomerServiceImpl.java
+++ Car _Rental App/FileUploadBackEnd/src/main/java/lk/ijse/spring/service/CustomerServiceImpl.java
@@ -10,27 +10,37 @@ import java.util.List;
 
 @Service //put for the Application Context
 @Transactional // introduce aspect for Transaction manager
-public class CustomerServiceImpl {
+public class CustomerServiceImpl implements CustomerService{
 
     @Autowired
     private CustomerRepo customerRepo;
 
+    @Override
     public void saveCustomer(Customer entity){
-        customerRepo.save(entity);
+        if(!customerRepo.existsById(entity.getCustomerID())){
+            customerRepo.save(entity);
+        }else {
+            throw new RuntimeException("Customer already exist...");
+        }
+
     }
 
+    @Override
     public void deleteCustomer(String id){
         customerRepo.deleteById(id);
     }
 
+    @Override
     public void updateCustomer(Customer entity){
         customerRepo.save(entity);
     }
 
+    @Override
     public Customer searchCustomer(String id){
         return customerRepo.findById(id).get();
     }
 
+    @Override
     public List<Customer> getAllCustomers(){
         return customerRepo.findAll();
     }

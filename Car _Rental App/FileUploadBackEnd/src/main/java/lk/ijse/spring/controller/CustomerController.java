42013ee52ce665f8428b3267dd3107cda8f264ab
Amayuru Indeewara <indeewara5678@gmail.com>
2022-07-14 02:27:09 +0530

    added saveCustomer in CustomerController

--- Car _Rental App/FileUploadBackEnd/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ Car _Rental App/FileUploadBackEnd/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -3,10 +3,7 @@ package lk.ijse.spring.controller;
 import lk.ijse.spring.entity.Customer;
 import lk.ijse.spring.service.CustomerServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +19,10 @@ public class CustomerController {
     public List<Customer> getAllCustomers(){
 
         return customerService.getAllCustomers();
+    }
+
+    @PostMapping
+    public void saveCustomer(@ModelAttribute Customer customer){
+        customerService.saveCustomer(customer);
     }
 }

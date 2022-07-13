290815337497691767acc697c17dad38eb0d5b1c
Amayuru Indeewara <indeewara5678@gmail.com>
2022-07-14 01:58:29 +0530

    added special two annotations

--- Car _Rental App/FileUploadBackEnd/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ Car _Rental App/FileUploadBackEnd/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,5 +1,7 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.service.CustomerServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("avc/customer")
 @CrossOrigin
 public class CustomerController {
+
+    @Autowired
+    CustomerServiceImpl customerService;
 
     @GetMapping
     public String getAllCustomers(){
--- Car _Rental App/FileUploadBackEnd/src/main/java/lk/ijse/spring/service/CustomerServiceImpl.java
+++ Car _Rental App/FileUploadBackEnd/src/main/java/lk/ijse/spring/service/CustomerServiceImpl.java
@@ -1,13 +1,37 @@
 package lk.ijse.spring.service;
 
 import lk.ijse.spring.entity.Customer;
+import lk.ijse.spring.repo.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service //put for the Application Context
+@Transactional // introduce aspect for Transaction manager
 public class CustomerServiceImpl {
-    public void saveCustomer(){}
-    public void deleteCustomer(){}
-    public void updateCustomer(){}
-    public Customer searchCustomer(){return null;}
-    /*public List<Customer> getAllCustomers{}*/
+
+    @Autowired
+    private CustomerRepo customerRepo;
+
+    public void saveCustomer(Customer entity){
+        customerRepo.save(entity);
+    }
+
+    public void deleteCustomer(String id){
+        customerRepo.deleteById(id);
+    }
+
+    public void updateCustomer(Customer entity){
+        customerRepo.save(entity);
+    }
+
+    public Customer searchCustomer(String id){
+        return customerRepo.findById(id).get();
+    }
+
+    public List<Customer> getAllCustomers(){
+        return customerRepo.findAll();
+    }
 }

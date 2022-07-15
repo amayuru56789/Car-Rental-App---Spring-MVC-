f709ac18f6314c86019f6a16a458597e49a71ba7
Amayuru Indeewara <indeewara5678@gmail.com>
2022-07-15 11:21:42 +0530

    validate DamageServiceImpl class for damageCars

--- Car _Rental App/FileUploadBackEnd/src/main/java/lk/ijse/spring/service/Impl/DamageServiceImpl.java
+++ Car _Rental App/FileUploadBackEnd/src/main/java/lk/ijse/spring/service/Impl/DamageServiceImpl.java
@@ -18,26 +18,42 @@ public class DamageServiceImpl implements DamageService {
 
     @Override
     public void saveDamage(Dammage entity) {
-
+        if (!damageRepo.existsById(entity.getDamageId())){
+            damageRepo.save(entity);
+        }else {
+            throw new RuntimeException("Damage Car already exist...");
+        }
     }
 
     @Override
     public void deleteDamage(String id) {
-
+        if (damageRepo.existsById(id)){
+            damageRepo.deleteById(id);
+        }else {
+            throw new RuntimeException("Please check the Damage ID.. no Such as Damage Car...!");
+        }
     }
 
     @Override
     public void updateDamage(Dammage entity) {
-
+        if (damageRepo.existsById(entity.getDamageId())){
+            damageRepo.save(entity);
+        }else {
+            throw new RuntimeException("No Such As Damage Car to update...! Please check The Id...!");
+        }
     }
 
     @Override
     public Dammage searchDamage(String id) {
-        return null;
+        if (damageRepo.existsById(id)){
+            return damageRepo.findById(id).get();
+        }else {
+            throw new RuntimeException("No Damage Car for "+id+" ...!");
+        }
     }
 
     @Override
     public List<Dammage> getAllDamages() {
-        return null;
+        return damageRepo.findAll();
     }
 }

fdd8cb3c59eb24d966bcf035cd4dfcc8d2656d17
Amayuru Indeewara <indeewara5678@gmail.com>
2022-07-15 13:12:35 +0530

    complete crud in PaymentController for Car_Rental App

--- Car _Rental App/FileUploadBackEnd/src/main/java/lk/ijse/spring/controller/PaymentController.java
+++ Car _Rental App/FileUploadBackEnd/src/main/java/lk/ijse/spring/controller/PaymentController.java
@@ -1,13 +1,42 @@
 package lk.ijse.spring.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.spring.entity.Payment;
+import lk.ijse.spring.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("avc/pay")
 @CrossOrigin
 public class PaymentController {
 
+    @Autowired
+    PaymentService paymentService;
 
+    @GetMapping
+    public List<Payment> getAllPayments(){
+        return paymentService.getAllPayments();
+    }
+
+    @PostMapping
+    public void savePayment(@ModelAttribute Payment payment){
+        paymentService.savePayment(payment);
+    }
+
+    @PutMapping
+    public void updatePayment(@RequestBody Payment payment){
+        paymentService.updatePayment(payment);
+    }
+
+    @DeleteMapping(params = {"id"})
+    public void deletePayment(String id){
+        paymentService.deletePayment(id);
+    }
+
+    @GetMapping(path = "/{id}")
+    public Payment searchPayment(@PathVariable String id){
+        return paymentService.searchPayment(id);
+    }
 }
--- Car _Rental App/FileUploadBackEnd/src/main/java/lk/ijse/spring/service/Impl/PaymentServiceImpl.java
+++ Car _Rental App/FileUploadBackEnd/src/main/java/lk/ijse/spring/service/Impl/PaymentServiceImpl.java
@@ -4,9 +4,13 @@ import lk.ijse.spring.entity.Payment;
 import lk.ijse.spring.repo.PaymentRepo;
 import lk.ijse.spring.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
+@Transactional
 public class PaymentServiceImpl implements PaymentService {
 
     @Autowired

fd7e8c6e3a325664df0365fed425e38cda5b2379
Amayuru Indeewara <indeewara5678@gmail.com>
2022-07-15 11:34:52 +0530

    complete crud in DamageController for Car_Rental App

--- Car _Rental App/FileUploadBackEnd/src/main/java/lk/ijse/spring/controller/DamageController.java
+++ Car _Rental App/FileUploadBackEnd/src/main/java/lk/ijse/spring/controller/DamageController.java
@@ -1,11 +1,42 @@
 package lk.ijse.spring.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.spring.entity.Dammage;
+import lk.ijse.spring.service.DamageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("avc/damage")
 @CrossOrigin
 public class DamageController {
+
+    @Autowired
+    DamageService damageService;
+
+    @GetMapping
+    public List<Dammage> getAllDamage(){
+        return damageService.getAllDamages();
+    }
+
+    @PostMapping
+    public void saveDamage(@ModelAttribute Dammage dammage){
+        damageService.saveDamage(dammage);
+    }
+
+    @PutMapping
+    public void updateDamage(@RequestBody Dammage dammage){
+        damageService.updateDamage(dammage);
+    }
+
+    @DeleteMapping(params = {"id"})
+    public void deleteDamage(String id){
+        damageService.deleteDamage(id);
+    }
+
+    @GetMapping(path = "/{id}")
+    public Dammage searchDamage(@PathVariable String id){
+        return damageService.searchDamage(id);
+    }
 }

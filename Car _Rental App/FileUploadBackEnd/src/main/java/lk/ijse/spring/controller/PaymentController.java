0cd977fe57e832e3d9b7cc7a3e575f0c1938d66c
Amayuru Indeewara <indeewara5678@gmail.com>
2022-07-15 11:43:58 +0530

    added PaymentController & PaymentRepo for write queries in Payments

---
+++ Car _Rental App/FileUploadBackEnd/src/main/java/lk/ijse/spring/controller/PaymentController.java
@@ -0,0 +1,13 @@
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("avc/pay")
+@CrossOrigin
+public class PaymentController {
+
+
+}
